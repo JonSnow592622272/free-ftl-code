4202cd3623bbad20a8eea5e65bf56aa2c7110b65
jonsnow <592622272@qq.com>
2019-05-26 22:19:05 +0800

    优化

--- src/main/java/com/my/ffc/plugin/FfcPlugin.java
+++ src/main/java/com/my/ffc/plugin/FfcPlugin.java
@@ -2,11 +2,13 @@ package com.my.ffc.plugin;
 
 import freemarker.template.Configuration;
 import freemarker.template.Template;
+import freemarker.template.TemplateException;
 import org.mybatis.generator.api.IntrospectedColumn;
 import org.mybatis.generator.api.IntrospectedTable;
 import org.mybatis.generator.api.dom.java.Field;
 import org.mybatis.generator.api.dom.java.Interface;
 import org.mybatis.generator.api.dom.java.TopLevelClass;
+import org.mybatis.generator.config.Context;
 
 import java.io.File;
 import java.io.IOException;
@@ -20,62 +22,95 @@ import java.util.Map;
 
 public class FfcPlugin extends BasePlugin {
 
-    private List<String> list;
-
     private Configuration cfgTable = new Configuration(Configuration.VERSION_2_3_23);
 
     private Configuration cfgField = new Configuration(Configuration.VERSION_2_3_23);
 
-
     @Override
-    public boolean validate(List<String> list) {
+    public void setContext(Context context) {
         try {
-            this.list = list;
-            cfgTable.setDirectoryForTemplateLoading(new File("src/main/resources/ffcTemplate/defaultGroup/tableTemplate"));
-            //TODO            cfgField.setDirectoryForTemplateLoading(new File("src/main/resources/ffcTemplate/defaultGroup/tableTemplate"));
+            cfgTable.setDirectoryForTemplateLoading(new File(context.getProperty("tableTemplatePackage")));
+            cfgField.setDirectoryForTemplateLoading(new File(context.getProperty("fieldTemplatePackage")));
         } catch (IOException e) {
             throw new RuntimeException("error!!!!!!!!!!!!执行失败!!!!!!!!!!!!!!!!!!!", e);
         }
+
+    }
+
+    @Override
+    public boolean validate(List<String> list) {
         return true;
     }
 
     @Override
     public boolean modelFieldGenerated(Field field, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) {
-        readyGo(field, topLevelClass, introspectedColumn, introspectedTable, modelClassType, null, PluginTypeEnum.FIELD);
+        readyGo(PluginTypeEnum.FIELD, field, topLevelClass, introspectedColumn, introspectedTable, modelClassType, null);
         return false;
     }
-
 
     @Override
     public boolean clientGenerated(Interface anInterface, TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
-        readyGo(null, topLevelClass, null, introspectedTable, null, anInterface, PluginTypeEnum.TABLE);
+        readyGo(PluginTypeEnum.TABLE, null, topLevelClass, null, introspectedTable, null, anInterface);
         return false;
     }
 
-    private void readyGo(Field field, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType, Interface anInterface, PluginTypeEnum pluginTypeEnum) {
+    private void readyGo(PluginTypeEnum pluginTypeEnum, Field field, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType, Interface anInterface) {
+
+        // 建立数据模型（Map）
+        Map<String, Object> commonMap = new HashMap<>();
+        commonMap.put("field", field);
+        commonMap.put("class", topLevelClass);
+        commonMap.put("column", introspectedColumn);
+        commonMap.put("table", introspectedTable);
+        commonMap.put("classType", modelClassType);
+        commonMap.put("interface", anInterface);
+
+        switch (pluginTypeEnum) {
+            case FIELD:
+                processFildTemplate(commonMap);
+                break;
+            case TABLE:
+                processTableTemplate(commonMap);
+                break;
+
+            default:
+                throw new RuntimeException("未匹配到类型!!!!!!!!!!!!执行失败!!!!!!!!!!!!!!!!!!!");
+        }
+
+    }
+
+    private void processTableTemplate(Map<String, Object> commonMap) {
         try {
-
             Template template = cfgTable.getTemplate("aa.ftl");
-
-            // 建立数据模型（Map）fff
-            Map<String, Object> root = new HashMap<>();
-            root.put("field", field);
-            root.put("class", topLevelClass);
-            root.put("column", introspectedColumn);
-            root.put("table", introspectedTable);
-            root.put("classType", modelClassType);
-            root.put("interface", anInterface);
 
             // 获取输出流（指定到控制台（标准输出））
             Writer out = new OutputStreamWriter(System.out);
             // StringWriter out = new StringWriter();
             // System.out.println(out.toString());
             // 数据与模板合并（数据+模板=输出）
-            template.process(root, out);
+            template.process(commonMap, out);
             out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TemplateException e) {
+            throw new RuntimeException("error!!!!!!!!!!!!执行失败!!!!!!!!!!!!!!!!!!!", e);
+        }
+    }
 
+    private void processFildTemplate(Map<String, Object> commonMap) {
+        try {
+            Template template = cfgField.getTemplate("aa.ftl");
 
-        } catch (Exception e) {
+            // 获取输出流（指定到控制台（标准输出））
+            Writer out = new OutputStreamWriter(System.out);
+            // StringWriter out = new StringWriter();
+            // System.out.println(out.toString());
+            // 数据与模板合并（数据+模板=输出）
+            template.process(commonMap, out);
+            out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TemplateException e) {
             throw new RuntimeException("error!!!!!!!!!!!!执行失败!!!!!!!!!!!!!!!!!!!", e);
         }
     }
