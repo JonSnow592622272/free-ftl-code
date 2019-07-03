2eb8be40a9ea85e7c85b99bb31d3f1d01d115987
JonSnow592622272 <592622272@qq.com>
2019-07-03 17:19:23 +0800

    完善

--- src/main/java/com/my/fgc/util/FileUtils.java
+++ src/main/java/com/my/fgc/util/FileUtils.java
@@ -12,6 +12,8 @@ import java.security.AccessController;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class FileUtils {
 
@@ -113,6 +115,20 @@ public class FileUtils {
             }
         }
         return strs;
+    }
+
+    private static Pattern linePattern = Pattern.compile("_(\\w)");
+
+    /** 下划线转驼峰 */
+    public static String lineToHump(String str) {
+        str = str.toLowerCase();
+        Matcher matcher = linePattern.matcher(str);
+        StringBuffer sb = new StringBuffer();
+        while (matcher.find()) {
+            matcher.appendReplacement(sb, matcher.group(1).toUpperCase());
+        }
+        matcher.appendTail(sb);
+        return sb.toString();
     }
 
 }
