3bbceb9233efc782a3bcca5b50a46b89b1445a47
jonsnow <592622272@qq.com>
2019-12-29 23:33:37 +0800

    增加zhixueyun模板

---
+++ src/main/java/com/shotgun/my/fgc/startup/zhixueyun/StartUpZhixueyun.java
@@ -0,0 +1,25 @@
+package com.shotgun.my.fgc.startup.zhixueyun;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+import org.springframework.core.io.ClassPathResource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StartUpZhixueyun {
+    public static void main(String[] args) throws Exception {
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        ClassPathResource generatorConfigResource = new ClassPathResource(
+                "fgcDbConfig/zhixueyunConfig/generatorConfig.xml");
+//		File configFile = new File("generatorConfig.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(generatorConfigResource.getInputStream());
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+}
