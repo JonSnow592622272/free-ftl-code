3be3dc77bf44e8541684e861ac081a4413c9cae7
wulingming <wulingming@zhixueyun.com>
2020-03-19 20:49:18 +0800

    增加达梦数据库代码生成案例（DaMengGenerator.java）

--- src/main/java/com/shotgun/my/fgc/startup/primeval/DaMengGenerator.java
+++ src/main/java/com/shotgun/my/fgc/startup/primeval/DaMengGenerator.java
@@ -8,17 +8,26 @@ import org.springframework.core.io.ClassPathResource;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MyBatisGenerator {
+/**
+ * @author wulm
+ * @desc 原生mybatis-generator连接“达梦数据库”生成代码
+ * 注意达梦数据库中“模式名”就是“数据库名”，表名一定要大写，否则找不到表
+ *
+ * @date 2020/3/19 20:28
+ **/
+public class DaMengGenerator {
     public static void main(String[] args) throws Exception {
         List<String> warnings = new ArrayList<String>();
         boolean overwrite = true;
         ClassPathResource generatorConfigResource = new ClassPathResource(
-                "fgcDbConfig/primeval/mybatisDefault/generatorConfig.xml");
+                "fgcDbConfig/primeval/dameng/generatorConfig.xml");
 //		File configFile = new File("generatorConfig.xml");
         ConfigurationParser cp = new ConfigurationParser(warnings);
         Configuration config = cp.parseConfiguration(generatorConfigResource.getInputStream());
         DefaultShellCallback callback = new DefaultShellCallback(overwrite);
-        org.mybatis.generator.api.MyBatisGenerator myBatisGenerator = new org.mybatis.generator.api.MyBatisGenerator(config, callback, warnings);
+        org.mybatis.generator.api.MyBatisGenerator myBatisGenerator =
+                new org.mybatis.generator.api.MyBatisGenerator(
+                config, callback, warnings);
         myBatisGenerator.generate(null);
     }
 }
